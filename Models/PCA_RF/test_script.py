81e2fb83d4b2f49c6172f0a76df0d14f73ccf4a5
navdeep94 <navdeepsingh3094@gmail.com>
2022-06-16 00:10:07 -0700

    Modified PCA_RF Code

--- Models/PCA_RF/test_script.py
+++ Models/PCA_RF/test_script.py
@@ -15,24 +15,14 @@
 # +
 ## Importing Packages and Modules and necessary Libraries
 from pca_rf_aafc.PCA_RF import pca_rf_ecodistrict_model_validation_scoring
-<<<<<<< HEAD
-=======
-from pca_rf_aafc.PCA_RF import pca_rf_ecodistrict_model_validation_scoring
->>>>>>> b44d785239e892b487afffaff1037c679bf4a503
 import pandas as pd
 
 ## Importing Wrangled and Clean Dataset
 data=pd.read_csv("aafc_data.csv",index_col='Unnamed: 0')
 
-<<<<<<< HEAD
 try:
     ## Storing list of unique ecodistricts in the list
     ecodistricts = data['ECODISTRICT_ID'].unique()
-=======
-## Instantiating the object of the Class by passing data and Ecodistrict ID
-ecodistrict_id = 748
-test=pca_rf_ecodistrict_model_validation_scoring(data,ecodistrict_id)
->>>>>>> b44d785239e892b487afffaff1037c679bf4a503
 
     print("Enter Valid Ecodistrict ID: ")
 
@@ -53,7 +43,6 @@ test=pca_rf_ecodistrict_model_validation_scoring(data,ecodistrict_id)
         train_predicted_df = test.predicted_train_dataset()
         train_predicted_df.to_csv('Outputs/train_predicted_df.csv')
 
-<<<<<<< HEAD
         ## Show Predicted Crop Yield on Testing Dataset
         test_predicted_df = test.predicted_test_dataset()
         test_predicted_df.to_csv('Outputs/test_predicted_df.csv')
@@ -75,8 +64,3 @@ test=pca_rf_ecodistrict_model_validation_scoring(data,ecodistrict_id)
         print("Ecodistrict not present")
 except:
     print("Invalid Input")
-=======
-## Show Predicted Crop Yield on New Test Set
-new_data_predicted_df = test.score(data_to_score)
-new_data_predicted_df.to_csv('Outputs/new_data_predicted_df.csv')
->>>>>>> b44d785239e892b487afffaff1037c679bf4a503
